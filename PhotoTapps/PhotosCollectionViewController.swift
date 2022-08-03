a363ab046d0fcd65b21dda1e90ea70971a2ebd56
DoolotYndybaev <91968210+DoolotYndybaev@users.noreply.github.com>
2022-08-03 22:59:30 +0600

    Made Custom CollectionView Cell

--- PhotoTapps/PhotosCollectionViewController.swift
+++ PhotoTapps/PhotosCollectionViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class PhotosCollectionViewController: UICollectionViewController {
     
+    let photos = ["dog1", "dog2", "dog3", "dog4", "dog5", "dog6", "dog7", "dog8", "dog9", "dog10", "dog11", "dog12", "dog13", "dog14", "dog15"]
+    let itemsPerRow: CGFloat = 2
+    let sectionInserts = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,13 +26,15 @@ class PhotosCollectionViewController: UICollectionViewController {
     
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 15
+        return photos.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as! PhotoCell
         
-        cell.backgroundColor = .blue
+        let imageName = photos[indexPath.item]
+        let image = UIImage(named: imageName)
+        cell.docImageView.image = image
         
         return cell
     }
@@ -36,20 +42,19 @@ class PhotosCollectionViewController: UICollectionViewController {
 extension PhotosCollectionViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let itemsPerRow: CGFloat = 3
-        let paddingWidth = CGFloat(20) * (itemsPerRow + CGFloat(1))
+        let paddingWidth = sectionInserts.left * (itemsPerRow + CGFloat(1))
         let availableWidth = collectionView.frame.width - paddingWidth
         let widthPerItem = availableWidth / itemsPerRow
         return CGSize(width: widthPerItem, height: widthPerItem)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        return sectionInserts
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 20
+        return sectionInserts.left
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 20
+        return sectionInserts.left
     }
 }
